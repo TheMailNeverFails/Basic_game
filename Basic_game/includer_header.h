8da45736ea641ab9811c47d96caad022a3c6b778
UP2071094 <114817856+TheMailNeverFails@users.noreply.github.com>
2024-01-15 17:33:40 +0000

    Delete includer_header.h

--- Basic_game/includer_header.h
+++
@@ -1,5 +0,0 @@
-#pragma once //preprocessing directive
-#include <iostream>
-#include <string> //library for strings
-#include <windows.h> //library i used for the sleep function
-using namespace std;
