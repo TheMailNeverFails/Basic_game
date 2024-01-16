e924f62fe4856115e8e85b53e8e1c802d34591d1
UP2071094 <114817856+TheMailNeverFails@users.noreply.github.com>
2024-01-16 01:08:29 +0000

    player login
    fixed major issues but still need to make a loop back into the confirmation

--- Basic_game/Player_name.cpp
+++ Basic_game/Player_name.cpp
@@ -1,17 +1,35 @@
 #include <iostream> //library for input output commands
 #include <string> //library for strings
 #include <windows.h> //library i used for the sleep function
-using namespace std;
+#include <limits>
 #include "Player_name.h" //includes my header file
+using namespace std;
+bool YES_NO() {
+	cout << "Are you sure this is your name?  Yes or No?" << endl;
+	char user_input;
+	cin >> user_input;
+
+	return (user_input == 'Yes' || user_input == 'yes' || user_input == 'YES');
+	//cin >> Confirmation();
+}
+
+
+int Confirmation() {
+	if (YES_NO()) {
+		cout << "you have confirmed this is your username" << endl;
+	}
+	else {
+		cout << "you have not confirmed your username" << endl;
+		}
+		return 0;
+	}
 
 
 void EnterName() {
 	string variable_login_name;
 	cout << endl << "Please enter your firstname:" << endl;
-	cin >> variable_login_name; 			
-	cout << endl << "are you sure this is your name?";
-
+	cin >> variable_login_name;
+	YES_NO();
 	cout << endl << "Welcome, " << variable_login_name << endl;
-
 }
 
--- Basic_game/Player_name.h
+++ Basic_game/Player_name.h
@@ -1,6 +1,7 @@
 #include <iostream> //library for input output commands
 #include <string> //library for strings
 #include <windows.h> //library i used for the sleep function
+#include <limits>
 using namespace std;
 
 void EnterName();
